7c460426704c1d0e0267c83609bf7cde776bb971
Francesco Limpias <francescolimpias@gmail.com>
2023-02-01 16:51:41 +0100

    Added HTML structure to Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,30 @@
 @extends('layouts.page')
 
 @section('header')
-<title>Home</title>
+    <title>DC Home</title>
 @endsection
 
 @section('main')
-    <h1>Main H1</h1>
+    <main id="HomeMain" class="d-flex flex-column align-items-center">
+        <div class="hover d-flex">
+            <h4>
+                CURRENT SERIES
+            </h4>
+        </div>
+
+        <div class="gallery  my-5
+        d-flex">
+
+            COMICS GALLERY
+            <div class="card">
+                <img src="" alt="">
+            </div>
+        </div>
+
+        <div class="bottom mb-3">
+            <button class="btn btn-primary rounded-0">
+                LOAD MORE
+            </button>
+        </div>
+    </main>
 @endsection
