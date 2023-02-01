f1451265e5b06c0668ef5c3e067259d8c26b4b24
Francesco Limpias <francescolimpias@gmail.com>
2023-02-01 15:31:48 +0100

    Added header contents

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,84 @@
-<div class="header">
-    <h2>Header</h2>
-</div>
+<header id="AppHeader">
+
+    <link rel="stylesheet" href="{{ Vite::asset('resources/scss/components/header.scss') }}">
+
+    {{-- upper nav --}}
+    <nav class="webNav paged">
+        <ul>
+            <li>
+                <a href="">DC PowerVisa</a>
+            </li>
+            <li>
+                <a href="">Additional DC Sites</a>
+            </li>
+        </ul>
+    </nav>
+
+    {{-- nav --}}
+    <nav class="siteNav paged">
+        <div class="left">
+            <img src="{{ Vite::asset('resources/images/dc-logo.png')}}" alt="">
+        </div>
+
+        <ul>
+            <li>
+                <a href="">
+                    characters
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    comics
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    movies
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    tv
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    games
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    collectibles
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    videos
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    fans
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    news
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    shop
+                </a>
+            </li>
+        </ul>
+
+        <div class="left">
+            <input type="text" placeholder="search 🔎">
+        </div>
+    </nav>
+
+    <div class="jumbotron">
+        
+    </div>
+</header>
