3300aef534abd69ffe22ddfd57ad32ed6ca1b819
Francesco Limpias <francescolimpias@gmail.com>
2023-02-01 15:36:56 +0100

    Added footer content

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,157 @@
-<div class="footer">
-    <h2>Footer</h2>
+<div id="AppFooter">
+
+    <link rel="stylesheet" href="{{ Vite::asset('resources/scss/components/footer.scss') }}">
+
+    {{-- SiteMap --}}
+    <div class="siteMap paged">
+
+        <div class="left py-3">
+            <div class="maps mb-3">
+
+                <div>
+                    <nav>
+                        <header>
+                            <h6>DC COMICS</h6>
+                        </header>
+                        <ul>
+                            <li>
+                                <a href="">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Link
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <br>
+                    <nav>
+                        <header>
+                            <h6>SHOP</h6>
+                        </header>
+                        <ul>
+                            <li>
+                                <a href="">
+                                    Link
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Link
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <nav>
+                    <header>
+                        <h6>DC</h6>
+                    </header>
+                    <ul>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <nav>
+                    <header>
+                        <h6>SITES</h6>
+                    </header>
+                    <ul>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Link
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+            <span>Copyrights</span>
+        </div>
+
+        <div class="right">
+        </div>
+
+    </div>
+
+    {{-- Credits Section --}}
+    <div class="credits paged 
+    d-flex 
+    justify-content-between
+    align-items-center
+    py-3">
+
+        <div class="left">
+            <button class="btn btn-dark">SIGN UP NOW!</button>
+        </div>
+
+        <div class="right d-flex gap-1">
+            <span>
+                <b>
+                    FOLLOW US:
+                </b>
+            </span>
+            <ul class="d-flex gap-1">
+                <li><a href="">FB</a></li>
+                <li><a href="">TW</a></li>
+                <li><a href="">YT</a></li>
+                <li><a href="">PT</a></li>
+                <li><a href="">PS</a></li>
+            </ul>
+        </div>
+
+    </div>
 </div>
